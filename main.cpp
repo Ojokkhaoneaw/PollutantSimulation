3f16b1fa8abfa632aea019d76d79998336e5c0b4
Ojokkhaoneaw <jerayu766@gmail.com>
2022-04-21 02:59:34 +0700

    latest

--- main.cpp
+++ main.cpp
@@ -105,6 +105,8 @@ int main() {
     initialize(v,nx, ny,v_init);
     initialize(p,nx, ny,p_init);
     initialize(phi,nx,ny,0) ;
+    cout << "------------v-------------" <<"\n" ;
+    visualize(v,nx,ny);
     // West
     inflow_condition(u, v, nx, ny, 'w', u_init, v_init);
     pressure_condition(p, nx, ny, dx, dy, 'w', 'N', p_init);
@@ -121,26 +123,7 @@ int main() {
     noslip_condition(u, v, nx, ny, 's');
     pressure_condition(p, nx, ny, dx, dy, 's', 'N', p_init);
     phi_condition(phi,nx,ny,dx,dy,'s',phi_init) ;
-    cout << "------------u-------------" <<"\n" ;
-    visualize(u,nx,ny);
     for (int num_iter = 1; num_iter <= iter; num_iter++) {
-
-        compute_F(F, u, v, nx, ny, dx, dy, dt, gamma, Re, g_x);
-        compute_G(G, u, v, nx, ny, dx, dy, dt, gamma,Re, g_y);
-        compute_RHS(RHS, F, G, nx, ny, dx, dy, dt);
-
-        poisson(p, p_new, RHS, nx, ny, dx, dy, omega, eps, iter_max);
-        compute_uv(u, v, F, G, p_new, nx, ny, dx, dy, dt);
-        inflow_condition(u ,v , nx, ny, 'w', u_init, v_init);
-        outflow_condition(u ,v , nx, ny, 'e');
-        noslip_condition(u ,v , nx, ny, 'n');
-        noslip_condition(u ,v , nx, ny, 's');
-        compute_phi(phi,phi_new,u,v,nx,ny,dx,dy,dt,gamma,Re) ;
-        update(phi,phi_new,nx,ny) ;
-        phi_condition(phi,nx,ny,dx,dy,'w',phi_init) ;
-        phi_condition(phi,nx,ny,dx,dy,'e',phi_init) ;
-        phi_condition(phi,nx,ny,dx,dy,'n',phi_init) ;
-        phi_condition(phi,nx,ny,dx,dy,'s',phi_init) ;
         if (num_iter == 1 || num_iter%5 == 0) {
             cout << "time step : " << num_iter <<"\n" ;
             cout << "------------u-------------" <<"\n" ;
@@ -151,6 +134,23 @@ int main() {
             paraview(num_iter ,"v" , v, nx, ny, dx, dy) ;
             paraview(num_iter ,"phi" , phi, nx, ny, dx, dy);
         }
+        compute_F(F, u, v, nx, ny, dx, dy, dt, gamma, Re, g_x);
+        compute_G(G, u, v, nx, ny, dx, dy, dt, gamma,Re, g_y);
+        compute_RHS(RHS, F, G, nx, ny, dx, dy, dt);
 
+        poisson(p, p_new, RHS, nx, ny, dx, dy, omega, eps, iter_max);
+        compute_uv(u, v, F, G, p_new, nx, ny, dx, dy, dt);
+        inflow_condition(u ,v , nx, ny, 'w', u_init, v_init);
+        outflow_condition(u ,v , nx, ny, 'e');
+        noslip_condition(u ,v , nx, ny, 'n');
+        noslip_condition(u ,v , nx, ny, 's');
+        cout << "------------v-------------" <<"\n" ;
+        visualize(v,nx,ny);
+        compute_phi(phi,phi_new,u,v,nx,ny,dx,dy,dt,gamma,Re) ;
+        update(phi,phi_new,nx,ny) ;
+        phi_condition(phi,nx,ny,dx,dy,'w',phi_init) ;
+        phi_condition(phi,nx,ny,dx,dy,'e',phi_init) ;
+        phi_condition(phi,nx,ny,dx,dy,'n',phi_init) ;
+        phi_condition(phi,nx,ny,dx,dy,'s',phi_init) ;
     }
 }
